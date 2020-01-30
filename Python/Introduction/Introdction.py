a27b898afd0ce9ff8c3ce637ce561574fde5c9fa
Joseph Lupton <43359031+jlupdawg@users.noreply.github.com>
2020-01-30 15:06:55 -0600

    Changed to class

--- Python/Introduction/Introdction.py
+++ Python/Introduction/Introdction.py
@@ -1,47 +1,103 @@
-from time import sleep
+myChar = "A"
+myFloat = 101.1
 
-myString = "Hello World!"
-print(type(myString))
+class Introduction:
+    """
+    An Introductory Class
+    Functions:
+    __init__()
+    __continue__()
+    AddToCharArray(char)
+    variableTypes()
+    Printing()
+    ForLoops()
+    WhileLoops()
+    ListOperations()
+    Dictionaries()
+    
 
-myChar = 'A'
-print(type(myChar))
+    """
+    def __init__(self):
+        self.charArray = ['A']
 
-myCharArray = ['A', 'B', 'C']
-print(type(myCharArray))
+    def __continue__(self):
+        print()
+        input("Press Enter to Continue")
 
-myInt = 100
-print(type(myInt))
+    def AddToCharArray(self, input):
+        self.charArray.append(input)
+        
+    def variableTypes(self):
+        myString = "Hello World!"
+        print(type(myString))
 
-myFloat = 100.1
-print(type(myFloat))
+        myChar = 'A'
+        print(type(myChar))
 
-print(myString, myChar, end = '-')
-print(myInt, myFloat, sep = '*')
+        print(type(self.charArray))
 
+        myInt = 100
+        print(type(myInt))
 
-for x in myCharArray:
-    print(x, end = ' ')
-print()
+        myFloat = 100.1
+        print(type(myFloat))
 
+    def Printing():
+        global myChar
+        global myFloat 
+        print(myString, myChar, end = '-')
+        print(myInt, myFloat, sep = '*')
 
-for i in range(0,5):
-    print(i, end = ' ')
-print()
+    def ForLoops(self):
+        for x in self.charArray:
+            print(x, end = ' ')
+            print()
 
+        for i in range(0,5):
+            print(i, end = ' ')
+            print()
 
-w = 0
-while(w < 5):
-    if(w == 2):
-        w += 1
-        continue
-    elif(w == 4):
-        break
-    else:
-        print("Everything is based on tabs " + str(w))
-        w += 1
-    #Cannot use ++ or --
+    def WhileLoops(self):
+        w = 0
+        while(w < 5):
+            if(w == 2):
+                w += 1
+                continue
+            elif(w == 4):
+                break
+            else:
+                print("Everything is based on tabs " + str(w))
+                w += 1
+            #Cannot use ++ or --
 
-#SingleLine Comments
+    def ListOperations(self):
+        self.charArray.append('C')
+        self.charArray.insert('A', 3)
+        self.charArray.extend(['H', 'E', 'L' ,'L', 'O']) #same as list1 = list1 + list2
+        print("'A' is at element " + str(this.charArray.index('A')))
+        self.charArray.remove('A')
+        self.charArray.sort()
+        self.charArray.reverse()
+        print("The last element '" + this.charArray.pop(index)+ "' was removed")
+        print(self.charArray)
+        
+    def Dictionaries():
+        print("TODO")
+### END OF CLASS ###
+
+t = Introduction()
+print(
+    t.__doc__)
+
+while True:
+    t.variableTypes()
+    t.__continue__()
+    t.ForLoops()
+    t.__continue__()
+    t.AddToCharArray('B')
+    t.ForLoops()
+
+# Single Line Comments
 
 '''
 Multiline
@@ -50,10 +106,10 @@ Comments
 
 '''
 Switcher / Dictionary
-Functions
+Catching errors/exeptions
 Headers from other file locations (adding paths)
-classes
-input
+Splitting Files
+Type Conversions
 '''
 
     
