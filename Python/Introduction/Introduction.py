eafd05e468170ebde0aa018af67aac152bfad3e5
Joseph Lupton <43359031+jlupdawg@users.noreply.github.com>
2020-02-20 15:15:25 -0600

    Changed to Match PEP8
    Erased old file and changed code to meet PEP8 standards

--- Python/Introduction/Introduction.py
+++ Python/Introduction/Introduction.py
@@ -1,5 +1,43 @@
-myChar = "A"
-myFloat = 101.1
+my_char = "A"
+my_float = 101.1
+my_string = "changeMe"
+my_int = 5
+
+
+def printing():
+    print(my_string, my_char, end='-')
+    print(my_int, my_float, sep='*')
+
+
+def my_continue():
+    print()
+    input("Press Enter to Continue")
+    print()
+
+
+def while_loops():
+    w = 0
+    while w < 5:
+        if w == 2:
+            w += 1
+            continue
+        elif w == 4:
+            break
+        else:
+            print("Everything is based on tabs " + str(w))
+            w += 1
+        # Cannot use ++ or --
+
+
+def dictionaries():
+    my_dict = {
+        "Day": 30,
+        "Month": "January",
+        "Year": 2020
+    }
+    print(my_dict)
+    print(my_dict.get("Day"))
+
 
 class Introduction:
     """
@@ -7,138 +45,116 @@ class Introduction:
     Functions:
     __init__()
     __continue__()
-    AddToCharArray(char)
-    variableTypes()
-    Printing()
+    add_to_char_array(char)
+    variable_types()
+    printing()
     ForLoops()
-    WhileLoops()
-    ListOperations()
-    Dictionaries()
-    PrintCharArray
+    while_loops()
+    list_operations()
+    dictionaries()
+    print_char_array
 
     """
+
     def __init__(self):
         self.charArray = ['A']
 
-    def __continue__(self):
-        print()
-        input("Press Enter to Continue")
+    def variable_types(self):
+        global my_string
+        global my_char
+        global my_int
+        global my_float
+        my_string = "Hello World!"
+        print(type(my_string))
 
-    def VariableTypes(self):
-        myString = "Hello World!"
-        print(type(myString))
-
-        myChar = 'A'
-        print(type(myChar))
+        my_char = 'A'
+        print(type(my_char))
 
         print(type(self.charArray))
 
-        myInt = 100
-        print(type(myInt))
+        my_int = 100
+        print(type(my_int))
 
-        myFloat = 100.1
-        print(type(myFloat))
+        my_float = 100.1
+        print(type(my_float))
 
-    def AddToCharArray(self, myInput):
-        self.charArray.append(myInput)
-        
-    def Printing():
-        global myChar
-        global myFloat 
-        print(myString, myChar, end = '-')
-        print(myInt, myFloat, sep = '*')
+    def add_to_char_array(self, my_input):
+        self.charArray.append(my_input)
 
-    def ForLoops(self):
+    def for_loops(self):
         for x in self.charArray:
-            print(x, end = ' ')
+            print(x, end=' ')
             print()
 
-        for i in range(0,5):
-            print(i, end = ' ')
+        for i in range(0, 5):
+            print(i, end=' ')
             print()
 
-    def WhileLoops(self):
-        w = 0
-        while(w < 5):
-            if(w == 2):
-                w += 1
-                continue
-            elif(w == 4):
-                break
-            else:
-                print("Everything is based on tabs " + str(w))
-                w += 1
-            #Cannot use ++ or --
-
-    def ListOperations(self):
+    def list_operations(self):
         self.charArray.append('C')
-        self.charArray.insert('A', 3)
-        self.charArray.extend(['H', 'E', 'L' ,'L', 'O']) #same as list1 = list1 + list2
-        print("'A' is at element " + str(this.charArray.index('A')))
-        self.charArray.remove('A')
+        self.charArray.insert(len(self.charArray), 'Z')
+        self.charArray.extend(['H', 'E', 'L', 'L', 'O'])  # same as list1 = list1 + list2
+
+        try:
+            print("'A' is at element " + str(self.charArray.index('A')))
+        except ValueError:
+            print("Sorry A is no longer an element in the list")
+
+        try:
+            self.charArray.remove('A')
+        except ValueError:
+            print("Sorry A is no longer an element in the list")
+
         self.charArray.sort()
         self.charArray.reverse()
-        print("The last element '" + this.charArray.pop(index)+ "' was removed")
-        PrintCharArray()
-        
-    def Dictionaries(self):
-        Dict = {
-          "Day": 30,
-          "Month": "January",
-          "Year": 2020
-        }
-        print(thisdict)
+        print("The last element '" + self.charArray.pop(len(self.charArray) - 1) + "' was removed")
+        self.print_char_array()
 
-    def PrintCharArray(self):
+    def print_char_array(self):
         print(self.charArray)
-        
-### END OF CLASS ###
+
+
+# END OF CLASS #
 
 t = Introduction()
-print(
-    t.__doc__)
+print(t.__doc__)
 
 while True:
-    t.VariableTypes()
-    t.__continue__()
-    
-    t.AddToCharArray('B')
-    t.PrintCharArray()
-    t.__continue__()
+    print("Variable Types")
+    t.variable_types()
+    my_continue()
 
-    t.Printing()
-    t.__continue__()
+    # Single Line Comments
 
-    t.ForLoops()
-    t.__continue__()
+    print("add_to_char_array")
+    t.add_to_char_array('B')
+    t.print_char_array()
+    my_continue()
 
-    t.WhileLoops()
-    t.__continue__()
+    print("Printing")
+    printing()
+    my_continue()
 
-    t.ListOperations()
-    t.__continue__()
+    print("For Loops")
+    t.for_loops()
+    my_continue()
 
-    t.Dictionaries()
-    t.__continue__()
+    print("While Loops")
+    while_loops()
+    my_continue()
 
-    if(input("Would you like to repeat the lesson? y or n") != y):
+    print("List Operations")
+    t.list_operations()
+    my_continue()
+
+    '''
+    Multiline
+    Comments
+    '''
+
+    print("Dictonary")
+    dictionaries()
+    my_continue()
+
+    if input("Would you like to repeat the lesson? y or n: ") != "y":
         exit()
-
-# Single Line Comments
-
-'''
-Multiline
-Comments
-'''
-
-'''
-Catching errors/exeptions
-Headers from other file locations (adding paths)
-Splitting Files
-Type Conversions
-'''
-
-    
-
-
-
